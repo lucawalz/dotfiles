06f0ed5de012bc777bec6600321aa41945cfe39c
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-07-19 00:20:00 +0200

    feat(theme): adopt carbonfox for the editor, prompt, and monitor

--- nvim/.config/nvim/lua/config/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,15 +1,18 @@
 return {
 	{
-		"nyoom-engineering/oxocarbon.nvim",
+		"EdenEast/nightfox.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
+			require("nightfox").setup({
+				options = { terminal_colors = true },
+			})
 			vim.opt.background = "dark"
-			vim.cmd.colorscheme("oxocarbon")
+			vim.cmd.colorscheme("carbonfox")
 
 			vim.keymap.set("n", "<leader>ut", function()
 				vim.o.background = vim.o.background == "dark" and "light" or "dark"
-				vim.cmd.colorscheme("oxocarbon")
+				vim.cmd.colorscheme(vim.o.background == "dark" and "carbonfox" or "dayfox")
 			end, { desc = "Toggle light/dark theme" })
 		end,
 	},
