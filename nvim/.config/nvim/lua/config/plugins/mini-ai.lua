335356805c81e32f137d16e9a9fbe9f7bd155439
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-07-20 00:57:28 +0200

    fix(nvim): split mini.ai function call and treesitter definition keys

--- nvim/.config/nvim/lua/config/plugins/mini-ai.lua
+++ nvim/.config/nvim/lua/config/plugins/mini-ai.lua
@@ -1,5 +1,13 @@
 return {
   "echasnovski/mini.ai",
   event = { "BufReadPre", "BufNewFile" },
-  opts = {},
+  opts = function()
+    local gen_spec = require("mini.ai").gen_spec
+    return {
+      custom_textobjects = {
+        f = false,
+        F = gen_spec.function_call(),
+      },
+    }
+  end,
 }
